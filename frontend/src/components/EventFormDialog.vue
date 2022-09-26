e538b0e334efdb8aa7d13f7316d371ef547df343
junichi <tmdjnch0901@gmail.com>
2022-09-26 15:50:54 +0900

    予定作成画面の修正

--- frontend/src/components/EventFormDialog.vue
+++ frontend/src/components/EventFormDialog.vue
@@ -6,43 +6,50 @@
       </v-btn>
     </v-card-actions>
     <v-card-text>
-        <DialogSection icon="mdi-square" :color="color">
-    <v-text-field v-model="name" label="タイトル"></v-text-field> </DialogSection>
+      <DialogSection icon="mdi-square" :color="color"> <v-text-field v-model="name" label="タイトル"></v-text-field> </DialogSection>
       <DialogSection icon="mdi-clock-outline">
         <DateForm v-model="startDate" />
         <div v-show="!allDay">
-            <TimeForm v-model="startTime" />
-          </div>
-        <DateForm v-model="endDate" />
+          <TimeForm v-model="startTime" />
+        </div>
+        <span class="px-2">–</span>
+        <DateForm v-model="endDate" :isError="isInvalidDatetime" />
         <div v-show="!allDay">
-            <TimeForm v-model="endTime" />
-          </div>
-          <CheckBox v-model="allDay" label="終日" />
+          <TimeForm v-model="endTime" :isError="isInvalidDatetime" />
+        </div>
+      </DialogSection>
+      <DialogSection>
+        <CheckBox v-model="allDay" label="終日" class="ma-0 pa-0" />
       </DialogSection>
       <DialogSection icon="mdi-card-text-outline">
-          <TextForm v-model="description" />
-        </DialogSection>
-        <DialogSection icon="mdi-palette">
-          <ColorForm v-model="color" />
-        </DialogSection>
+        <TextForm v-model="description" />
+      </DialogSection>
+      <DialogSection icon="mdi-palette">
+        <ColorForm v-model="color" />
+      </DialogSection>
     </v-card-text>
     <v-card-actions class="d-flex justify-end">
-      <v-btn @click="submit">保存</v-btn>
+      <v-btn :disabled="isInvalid" @click="submit">保存</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import { validationMixin } from 'vuelidate';
+import { required } from 'vuelidate/lib/validators';
+
 import DialogSection from './DialogSection';
 import DateForm from './DateForm';
 import TimeForm from './TimeForm';
 import TextForm from './TextForm';
 import ColorForm from './ColorForm';
 import CheckBox from './CheckBox';
+import { isGreaterEndThanStart } from '../functions/datetime';
 
 export default {
   name: 'EventFormDialog',
+  mixins: [validationMixin],
   components: {
     DialogSection,
     DateForm,
@@ -61,8 +68,19 @@ export default {
     color: '',
     allDay: false,
   }),
+  validations: {
+    name: { required },
+    startDate: { required },
+    endDate: { required },
+  },
   computed: {
     ...mapGetters('events', ['event']),
+    isInvalidDatetime() {
+      return !isGreaterEndThanStart(this.startDate, this.startTime, this.endDate, this.endTime, this.allDay);
+    },
+    isInvalid() {
+      return this.$v.$invalid || this.isInvalidDatetime;
+    },
   },
   created() {
     this.startDate = this.event.startDate;
@@ -79,6 +97,9 @@ export default {
       this.setEvent(null);
     },
     submit() {
+      if (this.isInvalid) {
+        return;
+      }
       const params = {
         name: this.name,
         start: `${this.startDate} ${this.startTime || ''}`,
